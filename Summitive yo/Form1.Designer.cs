0e9f66b5216758947b5c46cc93aa8646ba4c946b
bronhigg <bronhigg@ed.amdsb.ca>
2016-09-29 13:13:12 -0400

    OK this time this is done, more things have been changed and code has been bunched together.

--- Summitive yo/Form1.Designer.cs
+++ Summitive yo/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.startButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(62, 165);
+            this.startButton.Location = new System.Drawing.Point(56, 165);
             this.startButton.Name = "startButton";
             this.startButton.Size = new System.Drawing.Size(165, 84);
             this.startButton.TabIndex = 0;
@@ -42,22 +44,45 @@
             this.startButton.UseVisualStyleBackColor = true;
             this.startButton.Click += new System.EventHandler(this.button1_Click);
             // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(82, 136);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(89, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "By- Mikayla Racz";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(82, 149);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(103, 13);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "September 29, 2016";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.startButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "horoscopeSummative";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label dateLabel;
     }
 }
 
--- Summitive yo/Form1.cs
+++ Summitive yo/Form1.cs
@@ -26,7 +26,7 @@ namespace Summitive_yo
         private void button1_Click(object sender, EventArgs e)
         {
 
-            Graphics formGraphics = this.CreateGraphics();
+            Graphics formGraphics = this.CreateGraphics(); //this block shows that the pens, brushes and text are made
             Pen drawPen = new Pen(Color.Silver, 5);
             SolidBrush drawBrush = new SolidBrush(Color.White);
             SolidBrush wordBrush = new SolidBrush(Color.Black);
@@ -35,103 +35,71 @@ namespace Summitive_yo
             Font newFont = new Font("Times New Roman", 8, FontStyle.Bold);
 
             startButton.Hide();
+            nameLabel.Hide();
+            dateLabel.Hide();
 
             Thread.Sleep(1000);
-
             formGraphics.Clear(Color.Navy);
-
             formGraphics.DrawString("The Zodiac Cancer", drawFont, drawBrush, 25, 100);
-
             Thread.Sleep(2000);
 
-            formGraphics.Clear(Color.SlateGray); //The 'stars' are created
-
+            formGraphics.Clear(Color.SlateGray); //The 'stars' are created and sounds are made as the stars are created
             formGraphics.FillEllipse(drawBrush, 106, 19, 10, 10);
-            SoundPlayer player = new SoundPlayer(Properties.Resources.beep);
+            SoundPlayer player = new SoundPlayer(Properties.Resources.beep); //Yes i know this isnt how to thread audio but too bad it works
             player.Play();
-
             Thread.Sleep(1000);
-
             player.Stop();
-
             Thread.Sleep(1000);
-
             formGraphics.FillEllipse(drawBrush, 113, 84, 10, 10);
             SoundPlayer playe = new SoundPlayer(Properties.Resources.beep);
             player.Play();
-
             Thread.Sleep(1000);
-
             player.Stop();
-
             Thread.Sleep(1000);
-
             formGraphics.FillEllipse(drawBrush, 110, 113, 10, 10);
             SoundPlayer players = new SoundPlayer(Properties.Resources.beep);
             player.Play();
-
             Thread.Sleep(1000);
-
             player.Stop();
-
             Thread.Sleep(1000);
-
             formGraphics.FillEllipse(drawBrush, 80, 168, 10, 10);
             SoundPlayer play = new SoundPlayer(Properties.Resources.beep);
             player.Play();
-
             Thread.Sleep(1000);
-
             player.Stop();
-
             Thread.Sleep(1000);
-
             formGraphics.FillEllipse(drawBrush, 172, 192, 10, 10);
             SoundPlayer playr = new SoundPlayer(Properties.Resources.beep);
             player.Play();
-
             Thread.Sleep(1000);
-
             player.Stop();
 
             Thread.Sleep(1000); //The lines alligning the 'stars' are made
-
             formGraphics.DrawLine(drawPen, 110, 25, 117, 88);
-           
             Thread.Sleep(1200);
-            
             formGraphics.DrawLine(drawPen, 117, 88, 114, 115);
-
             Thread.Sleep(1200);
-            
             formGraphics.DrawLine(drawPen, 114, 115, 84, 172);
-
             Thread.Sleep(1200);
-            
             formGraphics.DrawLine(drawPen, 114, 115, 174, 194);
-
             Thread.Sleep(1200);
 
             formGraphics.Clear(Color.Silver); //Info about this specific zodiac sign
-
             formGraphics.FillEllipse(starBrush, 106, 19, 10, 10);
-
             Thread.Sleep(500);
-
             formGraphics.FillEllipse(starBrush, 113, 84, 10, 10);
-
             Thread.Sleep(500);
-
             formGraphics.FillEllipse(starBrush, 110, 113, 10, 10);
-
             Thread.Sleep(500);
-
             formGraphics.FillEllipse(starBrush, 80, 168, 10, 10);
-
             Thread.Sleep(500);
-
             formGraphics.FillEllipse(starBrush, 172, 192, 10, 10);
+            SoundPlayer plyer = new SoundPlayer(Properties.Resources.rain);
+            player.Play();
+            Thread.Sleep(1500);
+            player.Stop();
 
+            //Info about the zodiac sign
             formGraphics.DrawString("Cancer, the fourth sign of the zodiac, is all about home.", newFont, wordBrush, 0, 100);
             formGraphics.DrawString("Those born under this sign are 'roots' kinds of people,", newFont, wordBrush, 0, 110);
             formGraphics.DrawString("and take great pleasure in the comforts of home and family.", newFont, wordBrush, 0, 120);
